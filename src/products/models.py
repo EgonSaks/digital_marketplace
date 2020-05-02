3ad92362643d7d1f20ee1f6b8606a93774d72a34
Egon <egonsaks@gmail.com>
2020-05-02 21:08:05 +0200

    Add model fields to products model

--- src/products/models.py
+++ src/products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=150)
+    description = models.TextField(max_length=200, null=True)
+    price = models.DecimalField(max_digits=100,
+                        decimal_places=2, null=True, blank=True)
+
 
     def __str__(self):
         return self.title
