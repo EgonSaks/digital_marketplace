282aa9b84fd43fa29d85f0a679aefd82ecfcd3bc
Egon <egonsaks@gmail.com>
2020-05-02 23:12:47 +0200

    Add ProductAdmin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "description", "price", "sales_price"]
+    search_fields = ["title", "description"]
+    list_filter = ["price"]
+    list_editable = ["sales_price"]
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
--- src/products/models.py
+++ src/products/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=200, null=True)
-    price = models.DecimalField(max_digits=100,
-                        decimal_places=2, null=True, blank=True)
-
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99, null=True)
+    sales_price = models.DecimalField(max_digits=100,decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural ='Products'
