cb8131156ac0b797b3aba68d18a74c138a90bf95
Egon <egonsaks@gmail.com>
2020-05-25 22:07:00 +0200

    Add product_post_save_reveiver

--- src/products/models.py
+++ src/products/models.py
@@ -12,7 +12,7 @@ def download_media_location(instance, filename):
 class Product(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     managers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='managers_product', blank=True)
-    media = models.FileField(blank=True,
+    media = models.ImageField(blank=True,
             null=True,
             upload_to=download_media_location,
             storage=FileSystemStorage(location=settings.PROTECTED_ROOT))
@@ -60,9 +60,15 @@ pre_save.connect(product_pre_save_reciever, sender=Product)
 def thumbnail_location(instance, filename):
     return "%s/%s" %(instance.product.slug, filename)
 
+THUMB_CHOICES = (
+    ("hd", "HD"),
+    ("sd", "SD"),
+    ("micro", "Micro"),
+)
+
 class Thumbnail(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=THUMB_CHOICES, default='hd')
     height = models.CharField(max_length=20, null=True, blank=True)
     width = models.CharField(max_length=20, null=True, blank=True)
     media = models.ImageField(
@@ -78,6 +84,86 @@ class Thumbnail(models.Model):
         class Meta:
             verbose_name = 'Thumbnail'
             verbose_name_plural = 'Thumbnails'
+import os
+import shutil
+from PIL import Image
+import random
+
+from django.core.files import File
+
+def product_post_save_receiver(sender, instance, created, *args, **kwargs):
+    if instance.media:
+        hd, hd_created =Thumbnail.objects.get_or_create(product=instance, type='hd')
+        sd, sd_created =Thumbnail.objects.get_or_create(product=instance, type='sd')
+        micro, micro_created=Thumbnail.objects.get_or_create(product=instance, type='micro')
+
+        hd_max =(400, 400)
+        sd_max =(200, 200)
+        micro_max =(50, 50)
+
+        if hd_created:
+            filename=os.path.basename(instance.media.path)
+            thumb=Image.open(instance.media.path)
+            thumb.thumbnail(hd_max, Image.ANTIALIAS)
+            temp_loc = "%s/%s/tmp" %(settings.MEDIA_ROOT, instance.slug)
+            if not os.path.exist(temp_loc):
+                os.makedirs(temp_loc)
+            temp_file_path = os.path.join(temp_loc, filename)
+            if os.path.exists(temp_file_path):
+                temp_path = os.path.join(temp_loc, "%s" %(random.random()))
+                os.makedirs(temp_path)
+                temp_file_path = os.path.join(temp_path, filename)
+
+            temp_image = open(temp_file_path, "w")
+            thumb.save(temp_image)
+            thumb_data = open(temp_file_path, "r")
+
+            thumb_file = File(thumb_data)
+            hd.media.save(filename, thumb_file)
+
+        if sd_created:
+            filename=os.path.basename(instance.media.path)
+            thumb=Image.open(instance.media.path)
+            thumb.thumbnail(sd_max, Image.ANTIALIAS)
+            temp_loc = "%s/%s/tmp" %(settings.MEDIA_ROOT, instance.slug)
+            if not os.path.exists(temp_loc):
+                os.makedirs(temp_loc)
+            temp_file_path = os.path.join(temp_loc, filename)
+            if os.path.exists(temp_file_path):
+                temp_path = os.path.join(temp_loc, "%s" %(random.random()))
+                os.makedirs(temp_path)
+                temp_file_path = os.path.join(temp_path, filename)
+
+            temp_image = open(temp_file_path, "w")
+            thumb.save(temp_image)
+            thumb_data = open(temp_file_path, "r")
+
+            thumb_file = File(thumb_data)
+            sd.media.save(filename, thumb_file)
+
+        if micro_created:
+            filename=os.path.basename(instance.media.path)
+            thumb=Image.open(instance.media.path)
+            thumb.thumbnail(micro_max, Image.ANTIALIAS)
+            temp_loc = "%s/%s/tmp" %(settings.MEDIA_ROOT, instance.slug)
+            if not os.path.exists(temp_loc):
+                os.makedirs(temp_loc)
+            temp_file_path = os.path.join(temp_loc, filename)
+            if os.path.exists(temp_file_path):
+                temp_path = os.path.join(temp_loc, "%s" %(random.random()))
+                os.makedirs(temp_path)
+                temp_file_path = os.path.join(temp_path, filename)
+
+            temp_image = open(temp_file_path, "w")
+            thumb.save(temp_image)
+            thumb_data = open(temp_file_path, "r")
+
+            thumb_file = File(thumb_data)
+            micro.media.save(filename, thumb_file)
+
+          #shutil-rmtree(temp_loc, ignore_errors=True)
+
+post_save.connect(product_post_save_receiver, sender=Product)
 
 class MyProducts(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
