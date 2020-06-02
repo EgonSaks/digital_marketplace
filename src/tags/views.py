c1a9b9e71b45c6aab5bc4c4ed1dc323536318884
Egon <egonsaks@gmail.com>
2020-06-02 10:45:15 +0200

    Add TagView Manager

--- src/tags/views.py
+++ src/tags/views.py
@@ -13,12 +13,8 @@ class TagDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
             context = super(TagDetailView, self).get_context_data(*args, **kwargs)
             if self.request.user.is_authenticated:
-                    analytic_obj, created = TagView.objects.get_or_create(
-                            user = self.request.user,
-                            tag = self.get_object()
-                            )
-                    analytic_obj.count += 1
-                    analytic_obj.save()
+                tags = self.get_object()
+                new_view = TagView.objects.add_count(self.request.user, tags)
             return context
 
 class TagListView(ListView):
