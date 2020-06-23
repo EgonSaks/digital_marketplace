036b10fdafe19859ccf4467d927e2aacefab84d4
Egon <egonsaks@gmail.com>
2020-06-23 17:58:36 +0200

    Add and List Seller Products final

--- src/sellers/urls.py
+++ src/sellers/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from products.views import (
+        ProductUpdateView,
         ProductCreateView,
         SellerProductListView
 )
@@ -15,5 +16,6 @@ urlpatterns = [
     path('', SellerDashboard.as_view(), name="dashboard"),
     path('transactions/', SellerTransactionListView.as_view(), name="transactions"),
     path('products/', SellerProductListView.as_view(), name="product_list"),#sellers:product_list
+    path('<int:pk>/edit/', ProductUpdateView.as_view(), name="product_edit"),
     path('products/add/', ProductCreateView.as_view(), name="product_create"),
 ]
