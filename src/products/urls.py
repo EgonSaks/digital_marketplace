ffcb05abae9a6cb4cc18317f3190decf1174caf3
Egon <egonsaks@gmail.com>
2020-07-03 21:02:24 +0200

    Add Vendor Pages urls to endor page

--- src/products/urls.py
+++ src/products/urls.py
@@ -13,7 +13,7 @@ from .views import (
 
 urlpatterns = [
     path('', ProductListView.as_view(), name="list"), #products:list
-    path('vendor/(?P<vendor_name>[\w.@+-]+)/', VendorListView.as_view(), name="vendor_list"),
+    path('vendor/<vendor_name>/', VendorListView.as_view(), name="vendor_list"),
     path('<int:pk>/', ProductDetailView.as_view(), name="detail"),
     path('<slug>/', ProductDetailView.as_view(), name="detail_slug"),
     path('<int:pk>/download/', ProductDownloadView.as_view(), name="download"),
